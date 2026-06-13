1fdea33431ea1a1074069867685edffe5a69b1ef
vivs271 <vivek-singh@hotmail.com>
2026-06-13 11:30:50 +0000

    name of ci

--- terraformfiles/provider.tf
+++ terraformfiles/provider.tf
@@ -18,7 +18,4 @@ resource "azurerm_subnet" "subnet" {
  name = "vivsin-subnet-cicd-workshop"
  resource_group_name = azurerm_resource_group.rg.name
  virtual_network_name = azurerm_virtual_network.vnet.name
- address_prefixes = ["10.0.1.0/24"]
-}
-
-resource "azurerm_"
+ address_prefixes = ["10.0.1.0/24
